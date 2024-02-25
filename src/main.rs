527148941a2943776ad2659198dd61034b7e5745
Agnes Martinsson <agnesdlmartinsson@gmail.com>
2024-02-25 14:51:43 +0100

    Refactor arg parsing

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,24 @@ use std::fs;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let file_path = &args[2];
+    let config = parse_config(&args);
 
-    let file_content = fs::read_to_string(file_path)
+    let file_content = fs::read_to_string(&config.file_path)
         .expect("Can't read file content");
 
-    println!("Searching for '{}'", query);
-    println!("In file '{}'", file_path);
+    println!("Searching for '{}'", config.query);
+    println!("In file '{}'", config.file_path);
     println!("File Content:\n{}", file_content);
+}
+
+struct Config {
+    query: String,
+    file_path: String
+}
+
+fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let file_path = args[2].clone();
+
+    Config { query, file_path }
 }
