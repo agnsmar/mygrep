8dcde5c329120002a1299af57b6122f39c208665
Agnes Martinsson <agnesdlmartinsson@gmail.com>
2024-02-25 14:22:59 +0100

    Read file content

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,16 @@
 use std::env;
+use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    dbg!(args);
+
+    let query = &args[1];
+    let file_path = &args[2];
+
+    let file_content = fs::read_to_string(file_path)
+        .expect("Can't read file content");
+
+    println!("Searching for '{}'", query);
+    println!("In file '{}'", file_path);
+    println!("File Content:\n{}", file_content);
 }
