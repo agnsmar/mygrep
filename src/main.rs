8737c0ab5833d1e01ce870b2f43398d2fa4a3d5a
Agnes Martinsson <agnesdlmartinsson@gmail.com>
2024-02-26 12:48:35 +0100

    Change error handling

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,27 @@
 use std::env;
+use std::error::Error;
 use std::fs;
 use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args).unwrap_or_else(|e| {
-        println!("Error parsing arguments: {e}");
-        process::exit(1)
-    });
+    let config = Config::new(&args).unwrap();
 
-    run(config)
+    if let Err(e) = run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
+    }
 }
 
-fn run(config: Config) -> () {
-    let file_content = fs::read_to_string(&config.file_path)
-        .expect("Can't read file content");
-
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let file_content = fs::read_to_string(&config.file_path)?;
+    
     println!("Searching for '{}'", config.query);
     println!("In file '{}'", config.file_path);
-    println!("File Content:\n{}", file_content);
+    println!("File Content:\n{file_content}");
+
+    Ok(())
 }
 
 struct Config {
