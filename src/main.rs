82e94c129253702da57e73199045e8320aeaf28f
Agnes Martinsson <agnesdlmartinsson@gmail.com>
2024-02-26 17:47:14 +0100

    Split error presentation

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,13 @@ use mygrep::Config;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args).unwrap();
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
 
     if let Err(e) = mygrep::run(config) {
-        println!("Application error: {e}");
+        eprintln!("Application error: {e}");
         process::exit(1);
     }
 }
