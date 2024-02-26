7d11920df013d060a6ca2127d4ffc60d9a37557d
Agnes Martinsson <agnesdlmartinsson@gmail.com>
2024-02-26 12:52:00 +0100

    Extract run code to lib.rs

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,15 @@
 use std::env;
-use std::error::Error;
-use std::fs;
 use std::process;
+
+use mygrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::new(&args).unwrap();
 
-    if let Err(e) = run(config) {
+    if let Err(e) = mygrep::run(config) {
         println!("Application error: {e}");
         process::exit(1);
     }
 }
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let file_content = fs::read_to_string(&config.file_path)?;
-    
-    println!("Searching for '{}'", config.query);
-    println!("In file '{}'", config.file_path);
-    println!("File Content:\n{file_content}");
-
-    Ok(())
-}
-
-struct Config {
-    query: String,
-    file_path: String
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() != 3 {
-            return Err("incorrect number of arguments")
-        }
-
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-    
-        Ok(Config { query, file_path })
-    }
-}
-
-
